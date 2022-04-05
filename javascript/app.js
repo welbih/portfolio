bda571be206e3612036ea48d72dfc36c33e00e11
Welber Marques <welbih@yahoo.com.br>
2022-04-05 00:06:02 -0300

    update layout

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,12 @@
 var alternador = document.querySelector('.js-botao-chaveador');
 
 alternador.onclick = function() {
-  var menu = document.querySelector('.js-menu');
+  var menu = document.querySelector('.js-menu');  
   menu.classList.toggle('menu--exibindo');
+
+  var icon = document.querySelector('.js-botao-chaveador');  
+  if(icon) {
+    icon.classList.toggle('botao-chaveador--fechar');
+    icon.classList.toggle('botao-chaveador');
+  }
 }
